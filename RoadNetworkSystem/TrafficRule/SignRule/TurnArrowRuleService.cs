ebbd3046db8ecc39e7ba64f33859104498edb5b6
niuzhm <niuzhm@163.com>
2016-01-17 15:45:22 +0800

    add

---
+++ RoadNetworkSystem/TrafficRule/SignRule/TurnArrowRuleService.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoadNetworkSystem.TrafficRule.SignRule
+{
+    class TurnArrowRuleService
+    {
+        OleDbConnection _conn;
+        public TurnArrowRuleService(OleDbConnection conn)
+        {
+            this._conn = conn;
+        }
+    }
+}
